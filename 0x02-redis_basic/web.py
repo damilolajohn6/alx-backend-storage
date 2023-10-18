cc3fab99fc3b7d70737d26c650357127ad26a7ed
damilolajohn6 <damilolajohn622@gmail.com>
2023-10-18 16:12:11 +0000

    web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,73 @@
 #!/usr/bin/env python3
+
 """
-Web file
+create a web cache
 """
+
 import requests
-import redis
+import time
 from functools import wraps
 
-store = redis.Redis()
+CACHE_EXPIRATION = 10  # seconds
+url_access_counts = {}  # Track URL access counts
 
+def cache_result(url):
+    def decorator_get_page(func):
+        cache = {}
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a Url is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        @wraps(func)
+        def wrapper(url):
+            if url in cache and time.time() - cache[url]['timestamp'] < CACHE_EXPIRATION:
+                return cache[url]['content']
 
-        count_key = "count:" + url
-        html = method(url)
+            content = func(url)
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+            # Update cache
+            cache[url] = {'content': content, 'timestamp': time.time()}
+            return content
 
+        return wrapper
 
-@count_url_access
+    return decorator_get_page
+
+@cache_result(url_access_counts)
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    global url_access_counts
+    if url in url_access_counts:
+        url_access_counts[url] += 1
+    else:
+        url_access_counts[url] = 1
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return f"Error: {e}"
+
+# Test the get_page function
+if __name__ == "__main__":
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.google.com"
+    fast_url = "https://www.example.com"
+
+    # Access the slow URL multiple times to trigger caching
+    for i in range(3):
+        print(f"Accessing slow URL ({slow_url})...")
+        slow_content = get_page(slow_url)
+        print(f"Content (slow): {slow_content}")
+
+    # Access the fast URL
+    print(f"Accessing fast URL ({fast_url})...")
+    fast_content = get_page(fast_url)
+    print(f"Content (fast): {fast_content}")
+
+    # Wait for cache expiration (11 seconds) and access slow URL again
+    time.sleep(11)
+    print("Cache expired. Accessing slow URL again...")
+    slow_content_cached = get_page(slow_url)
+    print(f"Content (slow, cached): {slow_content_cached}")
+
+    # Display URL access counts
+    print("URL access counts:")
+    for url, count in url_access_counts.items():
+        print(f"{url}: {count}")
